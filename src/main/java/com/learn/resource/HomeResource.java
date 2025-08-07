d129671a46c1df31c1b79c7235f2640db91f962a
Deepak Yadav <deepakyadav1331@gmail.com>
2025-08-07 16:38:16 +0530

    Auto format

--- src/main/java/com/learn/resource/HomeResource.java
+++ src/main/java/com/learn/resource/HomeResource.java
@@ -7,18 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeResource {
 
-	@GetMapping("/")
-	public String getAll() {
-		return "<h1>Welcome ! </h1>";
-	}
-	
-	@GetMapping("/user/{name}")
-	public String getUser(@PathVariable("name") final String name) {
-		return String.format("<h1>Welcome, %s ! </h1></p>Role: User </p>",name);
-	}
-	
-	@GetMapping("/admin/{name}")
-	public String getAdmin(@PathVariable("name") final String name) {
-		return String.format("<h1>Welcome, %s ! </h1></p>Role: Admin </p>",name);
-	}
+    @GetMapping("/")
+    public String getAll() {
+        return "<h1>Welcome ! </h1>";
+    }
+
+    @GetMapping("/user/{name}")
+    public String getUser(@PathVariable("name") final String name) {
+        return String.format("<h1>Welcome, %s ! </h1></p>Role: User </p>", name);
+    }
+
+    @GetMapping("/admin/{name}")
+    public String getAdmin(@PathVariable("name") final String name) {
+        return String.format("<h1>Welcome, %s ! </h1></p>Role: Admin </p>", name);
+    }
 }
